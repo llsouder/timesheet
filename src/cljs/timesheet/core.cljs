723bc9b8cd7e926da77f26485d6bec6b7c567ac2
Lonnie Souder <lonnie@souderfamily.net>
2017-05-09 09:19:05 -0400

    change core to match tutorial.

--- src/cljs/timesheet/core.cljs
+++ src/cljs/timesheet/core.cljs
@@ -1,10 +1,18 @@
-(ns timesheet.core)
-
-(defn mount-components []
-  (let [content (js/document.getElementById "app")]
-    (while (.hasChildNodes content)
-      (.removeChild content (.-lastChild content)))
-    (.appendChild content (js/document.createTextNode "Welcome to timesheet"))))
+(ns timesheet.core
+  (:require-macros [hiccups.core :as hiccups :refer [html]]))
+  
+defn get-app []
+   (or (.-app js/window)
+       (set! (.-app js/window) #js {})))
 
 (defn init! []
-  (mount-components))
+  (let [app (get-app)
+        c (.Component (.-core js/ng)
+                      #js {:selector "my-app"
+                           :template (html [:div
+                                            [:h1 "My first Angular 2 app"]
+                                            [:div [:h2 "test"]]
+                                            [:div [:h3 "test2"]]])})
+        c (.Class c #js {:constructor (fn [])})]
+    (set! (.-AppComponent app) c)))
+
