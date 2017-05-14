07be66495901f1825733c7d2d582ffb9083de900
Lonnie Souder <lonnie@souderfamily.net>
2017-05-14 07:58:33 -0400

    Add employee and work_group tables.

--- resources/migrations/20170513221513-add-users-table.down.sql
+++ resources/migrations/20170513221513-add-users-table.down.sql
@@ -1,1 +1,5 @@
-DROP TABLE users;
+DROP TABLE employee;
+--;;
+DROP TABLE work_group;
+--;;
+DROP TABLE employee_in_group;
--- resources/migrations/20170513221513-add-users-table.up.sql
+++ resources/migrations/20170513221513-add-users-table.up.sql
@@ -1,9 +1,25 @@
-CREATE TABLE users
-(id VARCHAR(20) PRIMARY KEY,
+CREATE TABLE IF NOT EXISTS employee
+(employee_number VARCHAR(20) PRIMARY KEY,
+ email VARCHAR(30),
  first_name VARCHAR(30),
  last_name VARCHAR(30),
- email VARCHAR(30),
+ dob DATE,
+ street VARCHAR(30),
+ city VARCHAR(30),
+ state VARCHAR(2),
+ zip VARCHAR(10),
  admin BOOLEAN,
  last_login TIME,
  is_active BOOLEAN,
  pass VARCHAR(300));
+--;;
+CREATE TABLE IF NOT EXISTS work_group
+(group_id INT auto_increment primary key,
+name VARCHAR(30));
+--;;
+CREATE TABLE IF NOT EXISTS employee_in_group
+(employee_number VARCHAR(20),
+group_id INT,
+foreign key (employee_number) references employee,
+foreign key (group_id) references work_group,
+primary key (employee_number, group_id));
