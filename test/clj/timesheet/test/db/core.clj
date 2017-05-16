36886bf2d01ff3bb8caa52b17454ce0daa23e0b7
Lonnie Souder <lonnie@souderfamily.net>
2017-05-16 09:55:08 -0400

    Add test for create-employee! and check-employee-number.

--- test/clj/timesheet/test/db/core.clj
+++ test/clj/timesheet/test/db/core.clj
@@ -15,22 +15,45 @@
     (migrations/migrate ["migrate"] (select-keys env [:database-url]))
     (f)))
 
-(deftest test-users
-  (jdbc/with-db-transaction [t-conn *db*]
+(deftest test-check-employee-number
+  (jdbc/with-db-transaction[t-conn *db*]
     (jdbc/db-set-rollback-only! t-conn)
-    (is (= 1 (db/create-user!
+    (is (= '({:rows 0}) (db/check-employee-number t-conn {:employee_number 1}))) 
+    (is (= 1 (db/create-employee!
                t-conn
-               {:id         "1"
+               {:employee_number         "1"
                 :first_name "Sam"
                 :last_name  "Smith"
-                :email      "sam.smith@example.com"
-                :pass       "pass"})))
-    (is (= {:id         "1"
+                ;;:email      "sam.smith@example.com"
+                ;;:pass       "pass"
+                :dob "2020-03-01"})))
+    (is (= '({:rows 1}) (db/check-employee-number t-conn {:employee_number 1})))))
+
+(deftest test-employee
+  (jdbc/with-db-transaction [t-conn *db*]
+    (jdbc/db-set-rollback-only! t-conn)
+    (is (= 1 (db/create-employee!
+               t-conn
+               {:employee_number         "1"
+                :first_name "Sam"
+                :last_name  "Smith"
+                ;;:email      "sam.smith@example.com"
+                ;;:pass       "pass"
+                :dob "2020-03-01"})))
+    (is (= {:employee_number         "1"
             :first_name "Sam"
             :last_name  "Smith"
-            :email      "sam.smith@example.com"
-            :pass       "pass"
+            ;;:email      "sam.smith@example.com"
+            :email      nil
+            ;;:pass       "pass"
+            :pass       nil
+            :street       nil
+            :city       nil
+            :state       nil
+            :zip       nil
             :admin      nil
             :last_login nil
-            :is_active  nil}
-           (db/get-user t-conn {:id "1"})))))
+            :is_active  nil
+            :dob #inst "2020-03-01T05:00:00.000-00:00"}
+           (db/get-employee t-conn {:employee_number "1"})))
+    ))
