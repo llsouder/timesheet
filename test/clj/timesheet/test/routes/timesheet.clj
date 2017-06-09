0ee82ca4d2c06bd17ab9f3557ba9d5d843ed48d3
Lonnie Souder <lonnie@souderfamily.net>
2017-06-09 08:15:14 -0400

    Add a test for today is sunday and another for last year and over a month boundary too.  These are not any paticular edge cases.  I picked some arbitrary dates which use all the branches of logic.

--- test/clj/timesheet/test/routes/timesheet.clj
+++ test/clj/timesheet/test/routes/timesheet.clj
@@ -3,7 +3,17 @@
             [clj-time.core :as t]
             [timesheet.routes.timesheet :refer :all]))
 
-(deftest test-first-day-of-week
-  (testing "first day of week function on 6/7/2017 so 6/4/2017 is sunday."
+(deftest test-first-day-of-week-wed
+  (testing "first day of week function on 6/7/2017, Wed, so 6/4/2017 is sunday."
     (let [thedate (t/date-time 2017 06 7)]
     (is (= 4 (t/day (first-day-of-week thedate)))))))
+
+(deftest test-first-day-of-week-sun
+  (testing "first day of week function on 6/4/2017 so 6/4/2017 is sunday."
+    (let [thedate (t/date-time 2017 06 4)]
+    (is (= 4 (t/day (first-day-of-week thedate)))))))
+
+(deftest test-first-day-of-week-last-year
+  (testing "first day of week function on 6/4/2016 so 5/29/2016 is sunday."
+    (let [thedate (t/date-time 2016 06 4)]
+    (is (= 29 (t/day (first-day-of-week thedate)))))))
