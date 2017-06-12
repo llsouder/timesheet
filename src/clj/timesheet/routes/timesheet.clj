07e831f183e20d3e93e4446e9636a322a1b3ae85
Lonnie Souder <lonnie@souderfamily.net>
2017-06-11 21:33:36 -0400

    Add "Period End Date" with non functioning navigation buttons.

--- src/clj/timesheet/routes/timesheet.clj
+++ src/clj/timesheet/routes/timesheet.clj
@@ -10,7 +10,8 @@
             [clj-time.format :as f]
             [clj-time.core :as t]))
 
-(def formatter (f/formatter "MM-dd"))
+(def MM-dd-formatter (f/formatter "MM-dd"))
+(def MM-dd-yyyy-formatter (f/formatter "MM-dd-yyyy"))
 
 (def day-names '("days are not 0 indexed so 0 is this :)" "Monday" "Tuesday" "Wednesday" "Thursday" "Friday" "Saturday" "Sunday"))
 
@@ -24,6 +25,13 @@
     date
     (.withDayOfWeek (.minusWeeks date 1) 7)))
 
+(defn last-day-of-week [date]
+  "Return the last day of the week."
+    (t/plus (first-day-of-week date) (t/days 6)))
+
+(defn formatted-end-date [date]
+  (f/unparse MM-dd-yyyy-formatter (last-day-of-week (t/now))))
+
 (defn work-week [date]
   "Return seven days of the week starting on Sunday."
   (let [firstday (first-day-of-week date)]
@@ -31,12 +39,13 @@
 
 (defn formatted-work-week [date]
   "Return seven days starting on Sunday. Format is name, <br>,  MM-dd."
-  (map #(str (day-name %) "<br>" (f/unparse formatter %)) (work-week date)))
+  (map #(str (day-name %) "<br>" (f/unparse MM-dd-formatter %)) (work-week date)))
 
 (defn timesheet-page [{:keys [flash]}]
+  (let [date (t/now)]
   (layout/render
    "timesheet.html"
-   {:days (formatted-work-week (t/now)) }))
+   {:enddate (formatted-end-date date) :days (formatted-work-week date) })))
    
 
 (defroutes timesheet-routes
