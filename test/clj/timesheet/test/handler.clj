863aa26333f140625d572deaf8150491415ca7e6
Lonnie Souder <lonnie.ctr.souder@faa.gov>
2017-07-20 19:12:26 -0400

    More coverage in cloverage by running each route. Could not get the anitforgery working in test.

--- test/clj/timesheet/test/handler.clj
+++ test/clj/timesheet/test/handler.clj
@@ -3,11 +3,20 @@
             [ring.mock.request :refer :all]
             [timesheet.handler :refer :all]))
 
-(deftest test-app
+(deftest test-home
   (testing "main route"
     (let [response ((app) (request :get "/"))]
+      (is (= 200 (:status response)))))
+
+  (testing "about route"
+    (let [response ((app) (request :get "/about"))]
+      (is (= 200 (:status response)))))
+
+  (testing "timesheet route"
+    (let [response ((app) (request :get "/timesheet"))]
       (is (= 200 (:status response)))))
 
   (testing "not-found route"
     (let [response ((app) (request :get "/invalid"))]
       (is (= 404 (:status response))))))
+
