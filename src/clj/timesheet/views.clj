60ad4fb6df9e63944d1ce7e1e374071f09068296
Lonnie Souder <lonnie@souderfamily.net>
2017-08-09 07:47:28 -0400

    Add more "base.html" to views with tests for the the functions.  Using lein test-refresh for feedback.

--- src/clj/timesheet/views.clj
+++ src/clj/timesheet/views.clj
@@ -2,12 +2,30 @@
   (:require [hiccup.core :as hc]
             [hiccup.page :as hp]))
 
+(defn active [page arg]
+  (if (= page arg)
+    "active"
+    ""))
 
-(def base [:head 
+(defn base [{:keys [:page :servlet-context]}] 
+  [:head 
     (hp/include-css "/assets/bootstrap/css/bootstrap.min.css")
     (hp/include-css "/assets/font-awesome/css/font-awesome.min.css")
     (hp/include-css "/css/screen.css")
-])
+    [:body [:nav {:class "navbar navbar-dark bg-primary" :role "navigation"}
+           [:button {:class "navbar-toggler hidden-sm-up"
+                  :type "button"
+                  :data-toggle "collapse"
+                  :data-target "#collapsing-navbar"}
+              "&#9776;" ]
+            [:div {:class "collapse navbar-toggleable-xs" :id "collapsing-navbar"}
+              [:a {:class "navbar-brand" :href "{{servlet-context}}/"} "timesheet"]
+              [:ul {:class="nav navbar-nav"}
+                  [:li {:class="nav-item"}
+                      [:a {:href servlet-context :class (str "nav-link " (active page "home.html")) }
+                      "home"]]]]]]])
+
+
 
 (def body1 [:div {:class "super"} "This is super."])
 
---
+++ test/clj/timesheet/test/views.clj
@@ -0,0 +1,16 @@
+(ns timesheet.test.views
+  (:require [clojure.test :refer :all]
+            [hiccup.page :as hp]
+            [timesheet.views :refer :all]))
+
+(deftest test-base
+  (testing "passing a map and get the base html with an active"
+    (is (.contains (hp/html5 (base {:page "home.html" :servlet-context ""})) "active"))))
+
+(deftest test-active
+  (testing "returns the string 'active' when the page is equal to arg"
+    (is (= "active" (active "home.html" "home.html")))))
+
+(deftest test-not-active
+  (testing "returns the string 'active' when the page is equal to arg"
+    (is (= "" (active "admin.html" "home.html")))))
