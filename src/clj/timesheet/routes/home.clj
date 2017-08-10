c7a33bb65597ad28cd3756c79d2963e2d91c2f89
Lonnie Souder <lonnie@souderfamily.net>
2017-08-10 08:29:46 -0400

    Use views.clj to render home page.

--- src/clj/timesheet/routes/home.clj
+++ src/clj/timesheet/routes/home.clj
@@ -4,12 +4,15 @@
             [ring.util.http-response :as response]
             [clojure.java.io :as io]
             [timesheet.db.core :as db]
+            [timesheet.views :as view]
+            [hiccup.core :as hc]
             [clojure.tools.logging :as log]
             [ring.util.response :refer [redirect]]
             [struct.core :as st]))
 
 (defn home-page []
-  (layout/render "home.html"))
+  ;;(layout/render "home.html"))
+  (hc/html (view/base {:page "home.html" :servlet-context ""})))
 
 (defn about-page []
   (layout/render "about.html"))
