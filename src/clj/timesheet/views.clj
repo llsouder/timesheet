2a67e7acb3e109eeca367fd89edcad6051d78f78
Lonnie Souder <lonnie@souderfamily.net>
2017-08-17 22:50:47 -0400

    views has the base (or header) which I already added to all the routes.

--- src/clj/timesheet/views.clj
+++ src/clj/timesheet/views.clj
@@ -1,10 +1,8 @@
 (ns timesheet.views
   (:require [hiccup.core :as hc]
             [hiccup.page :as hp]
-            [taoensso.timbre :as timbre
-             :refer [log  trace  debug  info  warn  error  fatal  report
-                     logf tracef debugf infof warnf errorf fatalf reportf
-                     spy get-env]]
+            [timesheet.dateutil :as dutil]
+            [clj-time.format :as f]
             [hiccup.page :as hp]))
 
 
@@ -42,8 +40,63 @@
              (page-link page "timesheet" servlet-context)
              (page-link page "about" servlet-context)]]]
     [:div {:class "container"}
-          html-page]]])
+     html-page]]])
 
+(defn charge-select [row charges]
+  [:select {:name (str "charge-row" row) :form "timesheet"}
+   ;;blank
+   [:option {:value "" }]
+   (for [charge charges]
+     [:option {:value (:id charge)} (:name charge)])])
+
+(defn timesheet [{:keys [enddate date charges]}]
+ [:div {:style "text-align:right"}
+ [:form {:method "POST", :action "/timesheet_next"}
+  [:input {:type "text", :style "display:none", :name "enddate", :value enddate}]
+  [:input {:type "submit", :class "btn btn-primary", :name "forward", :value "->"}]]
+ [:form {:method "POST", :action "/timesheet_back"} "Period End Date:" 
+  [:input {:type "text" :name "enddate" :style "displayenddate" :value enddate :disabled "disabled" }]
+  [:input {:type "text", :style "display:none", :name "enddate", :value enddate}]
+  [:input {:type "submit", :class "btn btn-primary", :name "backward", :value "<-"}]]]
+[:div {:class "row"}
+ [:div {:class "span12"}
+  [:input {:type "text", :style "display:none", :name "enddate", :value enddate}]
+  [:table {:class "timesheet fixed table table-bordered table-striped table-highlight"}
+   [:thead  
+    [:tr {:style "background-color:#0d47a1;color:#ffffff"}
+     [:th {:style "width: 180px;text-align: center;"} "Name"]
+     ;;(add-day-header date)
+     [:th {:style "width: 30px;text-align: center;"} "Total"]]]
+   [:form {:method "POST", :id "timesheet", :action "/timesheet_submit"}]
+   [:tbody
+    "<!--row1-->"
+    [:tr
+     [:td
+      (charge-select 1 charges)
+      [:td {:style "width: 30px;text-align: center; background-color:#606060; color: #ff0000 ;"}
+       [:input {:onblur "findTotal('row1')", :type "text", :name "row1-0", :size "4"}]" "]]
+     [:td {:style "width: 30px;text-align: center; background-color:#FFFFFF; color: #ff0000 ;"}
+      [:input {:onblur "findTotal('row1')", :type "text", :name "row1-1", :size "4"}]" "]
+     [:td {:style "width: 30px;text-align: center; background-color:#FFFFFF; color: #ff0000 ;"}
+      [:input {:onblur "findTotal('row1')", :type "text", :name "row1-2", :size "4"}]" "]
+     [:td {:style "width: 30px;text-align: center; background-color:#FFFFFF; color: #ff0000 ;"}
+      [:input {:onblur "findTotal('row1')", :type "text", :name "row1-3", :size "4"}]" "]
+     [:td {:style "width: 30px;text-align: center; background-color:#FFFFFF; color: #ff0000 ;"}
+      [:input {:onblur "findTotal('row1')", :type "text", :name "row1-4", :size "4"}]" "]
+     [:td {:style "width: 30px;text-align: center; background-color:#FFFFFF; color: #ff0000 ;"}
+      [:input {:onblur "findTotal('row1')", :type "text", :name "row1-5", :size "4"}]" "]
+     [:td {:style "width: 30px;text-align: center; background-color:#606060; color: #ff0000 ;"}
+      [:input {:onblur "findTotal('row1')", :type "text", :name "row1-6", :size "4"}]" "]"<!--total-->"
+     [:td {:style "width: 30px;text-align: center;"}
+      [:input {:type "text", :id "row1", :size "4"}]" "]]"<!--end of row1-->"  "<!--row2-->"
+    [:tr
+     [:td {:colspan "2"}
+      [:input {:type "text", :name "Signature", :value ""}]]
+     [:td {:colspan "5"}]
+     [:td {:colspan "2"}
+      [:input {:type "submit", :class "btn btn-primary", :name "submit", :value "Submit"}]]]]]]
+ [:script {:type "text/javascript"} "\nfunction findTotal(name){\n  var tot=0;\n  for (i = 0; i < 7; i++) {\n    var arr = document.getElementsByName(name + '-' + i)
+  [0];\n    if(parseInt(arr.value))\n      tot += parseInt(arr.value);\n    document.getElementById(name).value = tot;\n  }\n}"]])
 
 (defn add-base [page {:keys [body] :as all}]
   (assoc all :body (hp/html5 (base page "" body))))
